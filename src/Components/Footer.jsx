51d4c3a0c4493dc814905a6891358c82a4c0e0a0
Panchaya-M <Panchaya.meeboonpor@gmail.com>
2024-10-07 21:58:08 +0700

    setup layouts component
    layout component contains navbar, sidebar, and footer component

---
+++ src/Components/Footer.jsx
@@ -0,0 +1,5 @@
+function Footer() {
+  return <h1 className="text-h6Bold">Footer</h1>;
+}
+
+export default Footer;
---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,5 @@
+function Navbar() {
+  return <h1 className="text-h6Bold">Navbar</h1>;
+}
+
+export default Navbar;
