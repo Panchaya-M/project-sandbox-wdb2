2d8d40ff1d4222f4c98f5e6bb632f76b4fb6a6da
Sirima <sirimal2002@gmail.com>
2024-10-08 01:19:32 +0700

    test fork ui user.name

--- src/Components/Sidebar.jsx
+++ src/Components/Sidebar.jsx
@@ -1,5 +1,5 @@
 function Sidebar() {
-  return <h1 className="text-h6Bold">Sidebar jaa test test</h1>;
+  return <h1 className="text-h6Bold">Sidebar jaa test test test test</h1>;
 }
 
 export default Sidebar;
