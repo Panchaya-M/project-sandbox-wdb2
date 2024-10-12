6655186b7b6439807bad8fb1ced89c35a760cfd1
Sirima <sirimal2002@gmail.com>
2024-10-13 02:01:38 +0700

    add sub-sub-sidebar

--- src/Components/Sidebar.jsx
+++ src/Components/Sidebar.jsx
@@ -1,309 +1,117 @@
-// function Sidebar() {
-//   // return <h1 className="text-h6Bold">Sidebar jaa test test test test</h1>;
-//   return (
-//     <div className="w-[321px] h-screen rounded-r-[16px] bg-black text-white">
-//       <div className="mt-[20px]">
-//         <h1 className="text-2xl font-bold mb-6">Home</h1>
-//         <ul className="space-y-4">
-//           <li className="hover:bg-gray-800 p-2 rounded">Men</li>
-//           <li className="hover:bg-gray-800 p-2 rounded">Women</li>
-//           <li className="hover:bg-gray-800 p-2 rounded">Kids</li>
-//           <li className="hover:bg-gray-800 p-2 rounded">Shoes</li>
-//           <li className="hover:bg-gray-800 p-2 rounded">Accessories</li>
-//         </ul>
-//       </div>
-//     </div>
-//   );
-// }
-
-// export default Sidebar;
-
-// function Sidebar() {
-//   // return <h1 className="text-h6Bold">Sidebar jaa test test test test</h1>;
-//   return (
-//     <aside
-//       id="sidebar"
-//       className="fixed top-0 left-0 z-40 w-[321px] h-screen rounded-r-[16px] bg-black-700 text-white"
-//       aria-label="Sidebar"
-//     >
-//       <div className="h-full pt-[20px]  bg-gray-50 dark:bg-gray-800">
-//         <ul className="font-medium flex flex-col space-y-2 px-[32px]">
-//           <li className="flex items-center h-[48px]">
-//             <span class="w-full">Home</span>
-//           </li>
-//           <li className="flex items-center h-[48px]  justify-between">
-//             <span>Men</span>
-//             <a href="#">
-//               <svg
-//                 width="40"
-//                 height="40"
-//                 viewBox="0 0 40 40"
-//                 fill="none"
-//                 xmlns="http://www.w3.org/2000/svg"
-//               >
-//                 <path
-//                   fill-rule="evenodd"
-//                   clip-rule="evenodd"
-//                   d="M23.3995 20.5655L17.5562 26.4085C17.1965 26.7683 17.196 27.3502 17.5546 27.7089C17.9158 28.0701 18.4955 28.0669 18.8551 27.7073L25.3458 21.2166C25.3462 21.2163 25.3464 21.216 25.3466 21.2158C25.3469 21.2154 25.3472 21.2152 25.3475 21.215C25.5278 21.0346 25.6173 20.7998 25.617 20.5652C25.6161 20.3299 25.5265 20.0953 25.3475 19.9161C25.3472 19.9159 25.3469 19.9156 25.3466 19.9153C25.3464 19.915 25.3462 19.9148 25.3458 19.9145L18.8551 13.4235C18.4954 13.0638 17.9133 13.0632 17.5546 13.4219C17.1935 13.7831 17.1967 14.3629 17.5562 14.7223L23.3995 20.5655Z"
-//                   fill="#222222"
-//                 />
-//               </svg>
-//             </a>
-//           </li>
-//           <li className="flex items-center h-[48px]  justify-between">
-//             <span>Women</span>
-//             <a href="#">
-//               <svg
-//                 width="40"
-//                 height="40"
-//                 viewBox="0 0 40 40"
-//                 fill="none"
-//                 xmlns="http://www.w3.org/2000/svg"
-//               >
-//                 <path
-//                   fill-rule="evenodd"
-//                   clip-rule="evenodd"
-//                   d="M23.3995 20.5655L17.5562 26.4085C17.1965 26.7683 17.196 27.3502 17.5546 27.7089C17.9158 28.0701 18.4955 28.0669 18.8551 27.7073L25.3458 21.2166C25.3462 21.2163 25.3464 21.216 25.3466 21.2158C25.3469 21.2154 25.3472 21.2152 25.3475 21.215C25.5278 21.0346 25.6173 20.7998 25.617 20.5652C25.6161 20.3299 25.5265 20.0953 25.3475 19.9161C25.3472 19.9159 25.3469 19.9156 25.3466 19.9153C25.3464 19.915 25.3462 19.9148 25.3458 19.9145L18.8551 13.4235C18.4954 13.0638 17.9133 13.0632 17.5546 13.4219C17.1935 13.7831 17.1967 14.3629 17.5562 14.7223L23.3995 20.5655Z"
-//                   fill="#222222"
-//                 />
-//               </svg>
-//             </a>
-//           </li>
-//           <li className="flex items-center h-[48px]  justify-between">
-//             <span>Kids</span>
-//             <a href="#">
-//               <svg
-//                 width="40"
-//                 height="40"
-//                 viewBox="0 0 40 40"
-//                 fill="none"
-//                 xmlns="http://www.w3.org/2000/svg"
-//               >
-//                 <path
-//                   fill-rule="evenodd"
-//                   clip-rule="evenodd"
-//                   d="M23.3995 20.5655L17.5562 26.4085C17.1965 26.7683 17.196 27.3502 17.5546 27.7089C17.9158 28.0701 18.4955 28.0669 18.8551 27.7073L25.3458 21.2166C25.3462 21.2163 25.3464 21.216 25.3466 21.2158C25.3469 21.2154 25.3472 21.2152 25.3475 21.215C25.5278 21.0346 25.6173 20.7998 25.617 20.5652C25.6161 20.3299 25.5265 20.0953 25.3475 19.9161C25.3472 19.9159 25.3469 19.9156 25.3466 19.9153C25.3464 19.915 25.3462 19.9148 25.3458 19.9145L18.8551 13.4235C18.4954 13.0638 17.9133 13.0632 17.5546 13.4219C17.1935 13.7831 17.1967 14.3629 17.5562 14.7223L23.3995 20.5655Z"
-//                   fill="#222222"
-//                 />
-//               </svg>
-//             </a>
-//           </li>
-//           <li className="flex items-center h-[48px]  justify-between">
-//             <span>Shoes</span>
-//             <a href="#">
-//               <svg
-//                 width="40"
-//                 height="40"
-//                 viewBox="0 0 40 40"
-//                 fill="none"
-//                 xmlns="http://www.w3.org/2000/svg"
-//               >
-//                 <path
-//                   fill-rule="evenodd"
-//                   clip-rule="evenodd"
-//                   d="M23.3995 20.5655L17.5562 26.4085C17.1965 26.7683 17.196 27.3502 17.5546 27.7089C17.9158 28.0701 18.4955 28.0669 18.8551 27.7073L25.3458 21.2166C25.3462 21.2163 25.3464 21.216 25.3466 21.2158C25.3469 21.2154 25.3472 21.2152 25.3475 21.215C25.5278 21.0346 25.6173 20.7998 25.617 20.5652C25.6161 20.3299 25.5265 20.0953 25.3475 19.9161C25.3472 19.9159 25.3469 19.9156 25.3466 19.9153C25.3464 19.915 25.3462 19.9148 25.3458 19.9145L18.8551 13.4235C18.4954 13.0638 17.9133 13.0632 17.5546 13.4219C17.1935 13.7831 17.1967 14.3629 17.5562 14.7223L23.3995 20.5655Z"
-//                   fill="#222222"
-//                 />
-//               </svg>
-//             </a>
-//           </li>
-//           <li className="flex items-center h-[48px]  justify-between">
-//             <span>Accessories</span>
-//             <a href="#">
-//               <svg
-//                 width="40"
-//                 height="40"
-//                 viewBox="0 0 40 40"
-//                 fill="none"
-//                 xmlns="http://www.w3.org/2000/svg"
-//               >
-//                 <path
-//                   fill-rule="evenodd"
-//                   clip-rule="evenodd"
-//                   d="M23.3995 20.5655L17.5562 26.4085C17.1965 26.7683 17.196 27.3502 17.5546 27.7089C17.9158 28.0701 18.4955 28.0669 18.8551 27.7073L25.3458 21.2166C25.3462 21.2163 25.3464 21.216 25.3466 21.2158C25.3469 21.2154 25.3472 21.2152 25.3475 21.215C25.5278 21.0346 25.6173 20.7998 25.617 20.5652C25.6161 20.3299 25.5265 20.0953 25.3475 19.9161C25.3472 19.9159 25.3469 19.9156 25.3466 19.9153C25.3464 19.915 25.3462 19.9148 25.3458 19.9145L18.8551 13.4235C18.4954 13.0638 17.9133 13.0632 17.5546 13.4219C17.1935 13.7831 17.1967 14.3629 17.5562 14.7223L23.3995 20.5655Z"
-//                   fill="#222222"
-//                 />
-//               </svg>
-//             </a>
-//           </li>
-//         </ul>
-//       </div>
-//     </aside>
-//   );
-// }
-
-// export default Sidebar;
-
-// import React, { useState } from "react";
-
-// function Sidebar() {
-//   // State to track the selected item and toggle between main and sub-sidebar
-//   const [selectedItem, setSelectedItem] = useState(null);
-
-//   // List of main items and sub-items
-//   const items = [
-//     { id: 1, name: "Item 1", subItems: ["Subitem 1-1", "Subitem 1-2"] },
-//     { id: 2, name: "Item 2", subItems: ["Subitem 2-1", "Subitem 2-2"] },
-//   ];
-
-//   const handleItemClick = (item) => {
-//     setSelectedItem(item); // Select the item and show sub-sidebar
-//   };
-
-//   const handleBackClick = () => {
-//     setSelectedItem(null); // Go back to the main sidebar
-//   };
-
-//   return (
-//     <div className="flex">
-//       {/* Main Sidebar (hidden when an item is clicked) */}
-//       {!selectedItem && (
-//         <div className="w-64 bg-gray-800 text-black p-4">
-//           <h2 className="text-lg font-bold">Sidebar</h2>
-//           <ul className="mt-4">
-//             {items.map((item) => (
-//               <li
-//                 key={item.id}
-//                 className="cursor-pointer hover:bg-gray-700 p-2 rounded"
-//                 onClick={() => handleItemClick(item)}
-//               >
-//                 {item.name}
-//               </li>
-//             ))}
-//           </ul>
-//         </div>
-//       )}
-
-//       {/* Sub-sidebar (shown when an item is clicked) */}
-//       {selectedItem && (
-//         <div className="w-64 bg-gray-700 text-black p-4">
-//           <div className="flex justify-between items-center">
-//             <h2 className="text-lg font-bold">{selectedItem.name} Sub-items</h2>
-//             <button
-//               className="text-sm bg-gray-600 px-2 py-1 rounded"
-//               onClick={handleBackClick}
-//             >
-//               Back
-//             </button>
-//           </div>
-//           <ul className="mt-4">
-//             {selectedItem.subItems.map((subItem, index) => (
-//               <li key={index} className="p-2 hover:bg-gray-600 rounded">
-//                 {subItem}
-//               </li>
-//             ))}
-//           </ul>
-//         </div>
-//       )}
-//     </div>
-//   );
-// }
-
-// export default Sidebar;
-
-// import React, { useState } from "react";
-// import { FiArrowRight } from "react-icons/fi"; // Import the arrow icon from react-icons
-
-// function Sidebar() {
-//   // State to track the selected item and toggle between main and sub-sidebar
-//   const [selectedItem, setSelectedItem] = useState(null);
-
-//   // List of main items and sub-items
-//   const items = [
-//     { id: 1, name: "Item 1", subItems: ["Subitem 1-1", "Subitem 1-2"] },
-//     { id: 2, name: "Item 2", subItems: ["Subitem 2-1", "Subitem 2-2"] },
-//   ];
-
-//   const handleItemClick = (item) => {
-//     setSelectedItem(item); // Select the item and show sub-sidebar
-//   };
-
-//   const handleBackClick = () => {
-//     setSelectedItem(null); // Go back to the main sidebar
-//   };
-
-//   return (
-//     <div className="flex">
-//       {/* Main Sidebar (hidden when an item is clicked) */}
-//       {!selectedItem && (
-//         <div className="w-64 bg-limeGreen-800 text-black p-4">
-//           <h2 className="text-lg font-bold">Sidebar</h2>
-//           <ul className="mt-4">
-//             {items.map((item) => (
-//               <li
-//                 key={item.id}
-//                 className="flex justify-between items-center p-2 hover:bg-gray-700 rounded"
-//               >
-//                 <span>{item.name}</span>
-//                 {/* Arrow icon with click event */}
-//                 <FiArrowRight
-//                   className="cursor-pointer"
-//                   onClick={() => handleItemClick(item)}
-//                 />
-//               </li>
-//             ))}
-//           </ul>
-//         </div>
-//       )}
-
-//       {/* Sub-sidebar (shown when an item is clicked) */}
-//       {selectedItem && (
-//         <div className="w-64 bg-gray-700 text-black p-4">
-//           <div className="flex justify-between items-center">
-//             <h2 className="text-lg font-bold">{selectedItem.name} Sub-items</h2>
-//             <button
-//               className="text-sm bg-gray-600 px-2 py-1 rounded"
-//               onClick={handleBackClick}
-//             >
-//               Back
-//             </button>
-//           </div>
-//           <ul className="mt-4">
-//             {selectedItem.subItems.map((subItem, index) => (
-//               <li key={index} className="p-2 hover:bg-gray-600 rounded">
-//                 {subItem}
-//               </li>
-//             ))}
-//           </ul>
-//         </div>
-//       )}
-//     </div>
-//   );
-// }
-
-// export default Sidebar;
-
 import React, { useState } from "react";
 import ArrowRight from "../../src/assets/arrow_right.svg";
 import ArrowLeft from "../../src/assets/arrow_left.svg";
 
 function Sidebar() {
   const [selectedItem, setSelectedItem] = useState(null);
+  const [selectedSubItem, setSelectedSubItem] = useState(null);
 
+  // const items = [
+  //   { id: 1, name: "Men", subItems: ["Shirts", "Pants"] },
+  //   { id: 2, name: "Women", subItems: ["Dresses", "Tops"] },
+  //   { id: 3, name: "Kids", subItems: ["Toys", "Clothes"] },
+  //   { id: 4, name: "Shoes", subItems: ["Sneakers", "Boots"] },
+  //   { id: 5, name: "Accessories", subItems: ["Bags", "Belts"] },
+  // ];
   const items = [
-    { id: 1, name: "Men", subItems: ["Shirts", "Pants"] },
-    { id: 2, name: "Women", subItems: ["Dresses", "Tops"] },
-    { id: 3, name: "Kids", subItems: ["Toys", "Clothes"] },
-    { id: 4, name: "Shoes", subItems: ["Sneakers", "Boots"] },
-    { id: 5, name: "Accessories", subItems: ["Bags", "Belts"] },
+    {
+      id: 1,
+      name: "Men",
+      subItems: [
+        {
+          name: "Shirts",
+          subSubItems: ["Mock 1-1-1", "Mock 1-1-2"],
+        },
+        {
+          name: "Pants",
+          subSubItems: ["Mock1-2-1", "Mock 1-2-2"],
+        },
+      ],
+    },
+    {
+      id: 2,
+      name: "Women",
+      subItems: [
+        {
+          name: "Dresses",
+          subSubItems: ["Mock 2-1-1", "Mock 2-1-2"],
+        },
+        {
+          name: "Tops",
+          subSubItems: ["Mock2-2-1", "Mock 2-2-2"],
+        },
+      ],
+    },
+    {
+      id: 3,
+      name: "Kids",
+      subItems: [
+        {
+          name: "Toys",
+          subSubItems: ["Mock 1-1-1", "Mock 1-1-2"],
+        },
+        {
+          name: "Clothes",
+          subSubItems: ["Mock1-2-1", "Mock 1-2-2"],
+        },
+      ],
+    },
+    {
+      id: 4,
+      name: "Shoes",
+      subItems: [
+        {
+          name: "Sneakers",
+          subSubItems: ["Mock 2-1-1", "Mock 2-1-2"],
+        },
+        {
+          name: "Boots",
+          subSubItems: ["Mock 2-2-1", "Mock 2-2-2"],
+        },
+      ],
+    },
+    {
+      id: 5,
+      name: "Accessories",
+      subItems: [
+        {
+          name: "Bags",
+          subSubItems: ["Mock 2-1-1", "Mock 2-1-2"],
+        },
+        {
+          name: "Belts",
+          subSubItems: ["Mock 2-2-1", "Mock 2-2-2"],
+        },
+      ],
+    },
   ];
 
   const handleItemClick = (item) => {
-    setSelectedItem(item); // Select item and show sub-sidebar
+    setSelectedItem(item); // Select the item and show sub-sidebar
+    setSelectedSubItem(null); // Reset the sub-item selection
+  };
+
+  const handleSubItemClick = (subItem) => {
+    setSelectedSubItem(subItem); // Select the sub-item and show sub-sub-sidebar
   };
 
   const handleBackClick = () => {
+    setSelectedSubItem(null); // Go back to the sub-sidebar
+  };
+
+  const handleMainBackClick = () => {
     setSelectedItem(null); // Go back to the main sidebar
+    setSelectedSubItem(null); // Reset the sub-item selection
   };
 
   return (
     <aside
       id="sidebar"
-      className="fixed top-0 left-0 z-40 w-[321px] h-screen rounded-r-[16px]  text-black"
+      className="fixed top-0 left-0 z-40 w-[321px] h-screen rounded-r-[16px] text-black"
       aria-label="Sidebar"
     >
       <div className="h-full  bg-gray-50 dark:bg-gray-800">
-        {/* Main Sidebar */}
+        {/* Main Sidebar (hidden when an item is clicked) */}
         {!selectedItem && (
           <div className="pt-[20px]">
             <div className="font-medium flex flex-col px-[32px] ">
@@ -330,12 +138,12 @@ function Sidebar() {
           </div>
         )}
 
-        {/* Sub-sidebar */}
-        {selectedItem && (
+        {/* Sub-sidebar (shown when an item is clicked) */}
+        {selectedItem && !selectedSubItem && (
           <div className="pt-[16px]">
             <div className="font-medium flex flex-col">
               <div className="flex items-center h-[48px] mb-2 px-[16px] justify-between border-b border-b-black-300">
-                <a href="#" onClick={handleBackClick}>
+                <a href="#" onClick={handleMainBackClick}>
                   <img
                     src={ArrowLeft}
                     alt="Search"
@@ -350,15 +158,45 @@ function Sidebar() {
                     key={index}
                     className="flex items-center h-[48px] mb-2 justify-between"
                   >
-                    <span className="w-full">{subItem}</span>
+                    <span className="w-full">{subItem.name}</span>
                     {/* Arrow Icon */}
-                    <a href="#" onClick={() => handleItemClick(item)}>
+                    <a href="#" onClick={() => handleSubItemClick(subItem)}>
                       <img
                         src={ArrowRight}
                         alt="Search"
                         className="w-[40px] h-[40px]"
                       />
                     </a>
+                  </li>
+                ))}
+              </ul>
+            </div>
+          </div>
+        )}
+
+        {/* Sub-sub-sidebar (shown when a sub-item is clicked) */}
+        {selectedSubItem && (
+          <div className="pt-[16px]">
+            <div className="font-medium flex flex-col">
+              <div className="flex items-center h-[48px] mb-2 px-[16px] justify-between border-b border-b-black-300">
+                <a href="#" onClick={handleBackClick}>
+                  <img
+                    src={ArrowLeft}
+                    alt="Search"
+                    className="w-[40px] h-[40px]"
+                  />
+                </a>
+                <h2 className="text-h6Bold w-[225px]">
+                  {selectedSubItem.name}
+                </h2>
+              </div>
+              <ul className="px-[32px] text-subTitle">
+                {selectedSubItem.subSubItems.map((subSubItem, index) => (
+                  <li
+                    key={index}
+                    className="flex items-center h-[48px] pl-[10px] mb-2 justify-between hover:bg-limeGreen-700 transition-colors duration-200"
+                  >
+                    {subSubItem}
                   </li>
                 ))}
               </ul>
