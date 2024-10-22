739c753c38475330a46f5bb3c5e971b1bbe8c58f
Panchaya-M <Panchaya.meeboonpor@gmail.com>
2024-10-22 23:43:41 +0700

    fixed wrong component

--- src/main.jsx
+++ src/main.jsx
@@ -22,7 +22,7 @@ const router = createBrowserRouter([
         children: [
           {
             path: "/products/:category",
-            element: <ProductDetailPage />,
+            element: <ProductListPage />,
           },
         ]
       },
