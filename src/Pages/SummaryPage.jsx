500d311f47aaa3072e8f7baf92025d6bfa40a242
unknown <sirimal2002@gmail.com>
2024-10-28 03:01:02 +0700

    add subheader and delete some log

--- src/Pages/SummaryPage.jsx
+++ src/Pages/SummaryPage.jsx
@@ -7,7 +7,7 @@ import CartItem, { CartEmpty } from "../Components/UI/CartItem.jsx";
 import CartSummary, {
   CartSummaryEmpty,
 } from "../Components/UI/CartSummary.jsx";
-import SimilarProducts from "../Components/UI/SimilarProducts.jsx";
+import RandomProducts from "../Components/UI/RandomProducts.jsx";
 
 const VariantSection = ({ children, isEmpty }) => {
   return (
@@ -21,98 +21,6 @@ const VariantSection = ({ children, isEmpty }) => {
     </div>
   );
 };
-
-// function SelectBox({ type, name, items }) {
-//   return (
-//     <div className="relative">
-//       <select
-//         id={`${type}-${name}`}
-//         // defaultValue={defaultColor}
-//         className="block appearance-none w-full bg-white border border-gray-300 text-gray-700 py-2 px-3 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
-//       >
-//         {items.map((item) => (
-//           <option key={item} value={item}>
-//             {item}
-//           </option>
-//         ))}
-//       </select>
-//       <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
-//         <img src={Arrow} className="h-4 w-4" />
-//       </div>
-//     </div>
-//   );
-// }
-
-// function CartItem({
-//   image,
-//   name,
-//   colors,
-//   sizes,
-//   defaultColor,
-//   defaultSize,
-//   defaultQuantity,
-//   price,
-// }) {
-//   return (
-//     <div className="bg-white p-4 flex flex-col sm:flex-row items-start sm:items-center">
-//       <img
-//         src={image}
-//         alt={name}
-//         width={100}
-//         height={100}
-//         className=" mb-4 sm:mb-0 sm:mr-6 w-full sm:w-auto sm:h-32 object-cover"
-//       />
-//       <div className="flex-grow space-y-2 w-full">
-//         <div className="flex justify-between items-start">
-//           <h3 className="font-semibold text-lg">{name}</h3>
-//           <button className="text-gray-500 hover:text-red-500 p-1">
-//             <img src={Delete} alt="Delete" className="h-5 w-5" />
-//           </button>
-//         </div>
-
-//         <div className="flex flex-col sm:flex-row sm:items-center sm:justify-between space-y-2 sm:space-y-0 sm:space-x-2">
-//           <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-3 gap-2 w-full sm:w-auto">
-//             {/* color */}
-//             <div className="col-span-3 sm:col-span-1 md:col-span-1">
-//               <label
-//                 htmlFor={`color-${name}`}
-//                 className="text-sm font-medium text-gray-700 block mb-1"
-//               >
-//                 Color
-//               </label>
-//               <SelectBox type="color" name={name} items={colors} />
-//             </div>
-//             {/* size */}
-//             <div className="col-span-1 sm:col-span-1 md:col-span-1">
-//               <label
-//                 htmlFor={`size-${name}`}
-//                 className="text-sm font-medium text-gray-700 block mb-1"
-//               >
-//                 Size
-//               </label>
-//               <SelectBox type="size" name={name} items={sizes} />
-//             </div>
-//             {/* qty */}
-//             <div className="col-span-1 sm:col-span-1 md:col-span-1">
-//               <label
-//                 htmlFor={`quantity-${name}`}
-//                 className="text-sm font-medium text-gray-700 block mb-1"
-//               >
-//                 Qty.
-//               </label>
-//               <SelectBox type="quantity" name={name} items={[1, 2, 3, 4, 5]} />
-//             </div>
-//           </div>
-//           <div className="flex justify-end sm:justify-start items-center">
-//             <span className="font-semibold text-lg">
-//               THB {price.toFixed(2)}
-//             </span>
-//           </div>
-//         </div>
-//       </div>
-//     </div>
-//   );
-// }
 
 const cartItems = [
   {
@@ -188,7 +96,8 @@ export default function SummaryPage() {
 
           {/* Featured Products */}
           <div className="my-24">
-            <SimilarProducts />
+            <p className="text-h5Bold">People also like these</p>
+            <RandomProducts />
           </div>
         </div>
       </div>
