4315e85f643b46ddf6899a4573548b11461f35b4
Panchaya-M <Panchaya.meeboonpor@gmail.com>
2024-11-02 19:23:08 +0700

    import dropdown component

--- src/Components/UI/CartItem.jsx
+++ src/Components/UI/CartItem.jsx
@@ -5,8 +5,7 @@ import { Link } from "react-router-dom";
 import Button, { ButtonCustom } from "../UI/Button.jsx";
 import CardEmpty from "../../assets/images/empty_cart.png";
 import Delete from "../../assets/delete.svg";
-import Dropdown, { DropdownCustom } from "./Dropdown.jsx";
-import Arrow from "../../assets/arrow_down.svg";
+import Dropdown from "./Dropdown.jsx";
 import { updateItemInCart } from "../../api.js";
 
 // Define the custom size order
