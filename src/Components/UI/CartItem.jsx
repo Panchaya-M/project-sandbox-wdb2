06b8e6cedc15adedf6ad9a6845ecdd39a555dbbd
Ampere <vipawan.jau@gmail.com>
2024-11-02 22:07:36 +0900

    Fix sort issue

---
+++ src/Components/UI/CartItem.jsx
@@ -0,0 +1,278 @@
+import React, { useState } from "react";
+import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
+
+import Button, { ButtonCustom } from "../UI/Button.jsx";
+import CardEmpty from "../../assets/images/empty_cart.png";
+import Delete from "../../assets/delete.svg";
+import Dropdown from "./Dropdown.jsx";
+import Arrow from "../../assets/arrow_down.svg";
+import { updateItemInCart } from "../../api.js";
+
+// Define the custom size order
+const sizeOrder = ["S", "M", "L", "XL"];
+
+// // Sort the variants by size using the custom order
+// const sortedVariantBySize = (variants = []) => {
+//   if (sizeOrder.includes(variants[0])) {
+//     return variants?.sort(
+//       (a, b) => sizeOrder.indexOf(a) - sizeOrder.indexOf(b)
+//     );
+//   }
+
+//   return variants?.sort((a, b) => a - b);
+// };
+
+function SelectBox({
+  type,
+  name,
+  items,
+  defaultValue,
+  onChange,
+  disabledOptions = [],
+}) {
+  let isDisabled = false;
+  if (defaultValue === undefined) isDisabled = true;
+  if (items === undefined) items = [];
+
+  return (
+    <div className="relative">
+      <select
+        id={`${type}-${name}`}
+        defaultValue={defaultValue}
+        disabled={isDisabled}
+        onChange={onChange}
+        className={`block appearance-none w-full h-[54px]  border border-gray-300 text-gray-700 py-2 px-3 pr-8 rounded leading-tight focus:outline-none  focus:border-gray-500 ${
+          isDisabled ? "bg-gray-200" : "bg-white focus:bg-white"
+        }`}
+      >
+        {items.map((item, index) => (
+          <option
+            key={index}
+            value={item}
+            disabled={disabledOptions.includes(item)}
+          >
+            {item}
+          </option>
+        ))}
+      </select>
+      <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
+        <img src={Arrow} className="h-4 w-4" />
+      </div>
+    </div>
+  );
+}
+
+const CartItem = ({
+  item,
+  onColorChange,
+  onSizeChange,
+  onRemove,
+  cartId,
+  onQuantityChange,
+}) => {
+  const [selectedColor, setSelectedColor] = useState(item.defaultColor);
+  const [selectedSize, setSelectedSize] = useState(item.defaultSize);
+  const [selectedQuantity, setSelectedQuantity] = useState(
+    item.defaultQuantity
+  );
+
+  // Filter sizes for the selected color and disable those with remains = 0
+  const availableSizes = item.sizes;
+  const disabledSizes = item.variants
+    .filter(
+      (variant) => variant.color === selectedColor && variant.remains === 0
+    )
+    .map((variant) => variant.size);
+
+  // Filter colors for the selected size and disable those with remains = 0
+  const availableColors = item.colors;
+  const disabledColors = item.variants
+    .filter((variant) => variant.size === selectedSize && variant.remains === 0)
+    .map((variant) => variant.color);
+
+  // Update the cart based on color, size, and quantity selection
+  const updateCartItem = async (newColor, newSize, newQuantity) => {
+    const matchingVariant = item.variants.find(
+      (variant) => variant.color === newColor && variant.size === newSize
+    );
+
+    if (matchingVariant && matchingVariant.remains > 0) {
+      const body = {
+        skuCode: matchingVariant.skuCode,
+        quantity: newQuantity,
+      };
+
+      try {
+        await updateItemInCart(cartId, item.id, body);
+        console.log("Cart updated successfully");
+      } catch (error) {
+        console.error("Error updating cart:", error);
+      }
+    } else {
+      console.warn("No stock available for this color/size combination");
+    }
+  };
+
+  const handleColorChange = (e) => {
+    const newColor = e.target.value;
+    setSelectedColor(newColor);
+    updateCartItem(newColor, selectedSize, selectedQuantity);
+  };
+
+  const handleSizeChange = (e) => {
+    const newSize = e.target.value;
+    setSelectedSize(newSize);
+
+    updateCartItem(selectedColor, newSize, selectedQuantity);
+  };
+
+  const handleQuantityChange = (e) => {
+    console.log(">>>>>> ", e.target.value);
+
+    const newQuantity = Number(e.target.value);
+    onQuantityChange(newQuantity);
+    setSelectedQuantity(newQuantity);
+
+    // Pass the current color, size, and updated quantity to the updateCartItem function
+    updateCartItem(selectedColor, selectedSize, newQuantity);
+  };
+
+  return (
+    <div className="bg-white p-4 sm:p-6 rounded-lg shadow-sm">
+      <div className="flex flex-col sm:flex-row gap-4 sm:gap-6">
+        <div className="w-full sm:w-48 h-48 flex-shrink-0 overflow-hidden rounded-md">
+          <img
+            src={item.image}
+            alt={item.name}
+            className="w-full h-full object-cover hover:scale-105 transition-transform duration-300"
+          />
+        </div>
+        <div className="flex-1 flex flex-col justify-between">
+          <div className="flex justify-between items-start">
+            <h3 className="text-h6Bold hover:text-gray-700 line-clamp-2">
+              {item.name}
+            </h3>
+            <button
+              onClick={() => onRemove(item.id)}
+              className="text-gray-400 hover:text-red-500 transition-colors duration-200 ml-2"
+              aria-label="Remove item"
+            >
+              <img src={Delete} alt="Delete" className="h-10 w-10" />
+            </button>
+          </div>
+
+          <div className="flex flex-col items-end sm:flex-row sm:items-end sm:justify-between space-y-2 sm:space-y-0 sm:space-x-2">
+            <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-3 gap-2 w-full sm:w-auto">
+              <div className="col-span-2 sm:col-span-1 md:col-span-1">
+                <label
+                  htmlFor={`color-${item.id}`}
+                  className="text-bodyText text-gray-500 mb-1 block"
+                >
+                  Color
+                </label>
+                <SelectBox
+                  type="color"
+                  name={item.name}
+                  items={availableColors}
+                  defaultValue={selectedColor}
+                  onChange={handleColorChange}
+                  disabledOptions={disabledColors}
+                />
+              </div>
+
+              <div className="col-span-1 sm:col-span-1 md:col-span-1">
+                <label
+                  htmlFor={`size-${item.id}`}
+                  className="text-bodyText text-gray-500 mb-1 block"
+                >
+                  Size
+                </label>
+                <SelectBox
+                  type="size"
+                  name={item.name}
+                  items={availableSizes}
+                  defaultValue={selectedSize}
+                  onChange={handleSizeChange}
+                  disabledOptions={disabledSizes}
+                />
+              </div>
+
+              <div className="col-span-1 sm:col-span-1 md:col-span-1">
+                <label
+                  htmlFor={`quantity-${item.id}`}
+                  className="text-bodyText text-gray-500 mb-1 block"
+                >
+                  Quantity
+                </label>
+                <SelectBox
+                  type="quantity"
+                  name={item.name}
+                  items={item.quantities}
+                  defaultValue={selectedQuantity}
+                  onChange={handleQuantityChange}
+                />
+              </div>
+            </div>
+
+            <div className="flex flex-row items-end">
+              <p className="text-subHeading">
+                THB {item.price.toLocaleString()}.00
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export function CartEmpty() {
+  return (
+    <>
+      {/* item cart empty part */}
+      <div className="flex flex-col justify-center items-center text-center space-y-6 p-6">
+        <div>
+          <img
+            src={CardEmpty}
+            alt="Empty Cart"
+            className="mx-auto w-[403px] h-[403px]"
+          />
+        </div>
+        <div className="flex flex-col justify-center space-y-2">
+          <h1 className="text-h4Bold ">Your cart is empty</h1>
+          <p className="text-subHeading ">
+            Looks like you have not added anything to your cart.
+            <br />
+            Go ahead & explore top categories.
+          </p>
+        </div>
+        <div>
+          <Link to="/products" className="no-underline hover:no-underline">
+            <Button
+              text="Continue shopping"
+              customClassName="transition-colors duration-200"
+            ></Button>
+          </Link>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default CartItem;
+
+CartItem.propTypes = {
+  item: PropTypes.shape({
+    id: PropTypes.string.isRequired,
+    name: PropTypes.string.isRequired,
+    price: PropTypes.number.isRequired,
+    image: PropTypes.string.isRequired,
+    colors: PropTypes.arrayOf(PropTypes.string).isRequired,
+    sizes: PropTypes.arrayOf(PropTypes.string).isRequired,
+    quantities: PropTypes.arrayOf(PropTypes.number).isRequired,
+    defaultColor: PropTypes.string.isRequired,
+    defaultSize: PropTypes.string.isRequired,
+    defaultQuantity: PropTypes.number.isRequired,
+  }).isRequired,
+};
--- src/Pages/ProductList.jsx
+++ src/Pages/ProductList.jsx
@@ -1,9 +1,9 @@
-import { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import { useEffect, useRef, useState } from "react";
+import { useNavigate, useParams } from "react-router-dom";
 
-import DefaultLayout from "../Components/Layouts/DefaultLayout";
 import Arrow from "../assets/arrow_down.svg";
-import { getAllCategory, getProductByCategory } from "../api";
+import SortByIcon from "../assets/sortby.svg";
+import { getAllCategory, getChildrenCategories, getCollections, getParentCategory, getProductByCategory } from "../api";
 import { ProductCard } from "../Components";
 
 /**
@@ -22,29 +22,29 @@ import { ProductCard } from "../Components";
  * @param (permalink: string) onSelectFilter
  * @returns
  */
-const Filter = ({ categories, onSelectFilter }) => {
-  const [selected, setSelected] = useState("");
+const Filter = ({ items, onSelectFilter, selectedFilter }) => {
+  const [isOpen, setIsOpen] = useState(false);
 
   // On select filter
-  function onSelect(id, permalink) {
-    setSelected(id);
-
+  function onSelect(id, permalink, name) {
     // Redirect to category page
     if (typeof onSelectFilter === "function") {
-      onSelectFilter(permalink);
+      onSelectFilter(id, permalink, name);
     }
   }
 
   // List filter item
   const listFilterItem = (items) => {
+    if (items === undefined || !isOpen) return
+
     return items.map((item) => {
       return (
         <button
           key={`filter-item-${item.id}`}
           className={`${
-            selected === item.id ? "bg-limeGreen" : ""
+            selectedFilter === item.permalink ? "bg-limeGreen" : ""
           } p-2.5 text-sm block w-full text-left`}
-          onClick={() => onSelect(item.id, item.permalink)}
+          onClick={() => onSelect(item.id, item.permalink, item.name)}
         >
           {item.name}
         </button>
@@ -54,21 +54,42 @@ const Filter = ({ categories, onSelectFilter }) => {
 
   // List filter group
   const listFilterGroup = () => {
-    return categories.map((category) => {
+    return items.map((item) => {
       return (
-        <div key={`group-${category.name}`}>
+        <div key={`group-${item.name}`}>
           <div className="">
-            <p className="text-lg font-semibold py-3">{category.name}</p>
+            <button
+              className={`
+                  flex w-full text-left text-lg font-semibold py-3 px-2
+                  justify-between items-center
+                  ${selectedFilter === item.id ? "bg-limeGreen" : ""}
+                `}
+
+              onClick={() => {
+                if (item.children && item.children.length > 0) {
+                  setIsOpen(!isOpen);
+                } else {
+                  item.permalink && item.id && onSelect(item.id, item.permalink, item.name)
+                }
+              }}
+            >
+              {item.name}
+              {
+                item.children && item.children.length > 0 && (
+                  <img src={Arrow} alt="Arrow Down" className={`pl-4 relative transition-transform ${ isOpen ? 'rotate-180 translate-x-4' : 'rotate-0' }`} />
+                )
+              }
+            </button>
           </div>
-          <div className="">{listFilterItem(category.children)}</div>
+          <div className="pl-2">{listFilterItem(item.children)}</div>
         </div>
       );
     });
   };
 
-  // If categories is undefined
+  // If items is undefined
   // Show loading
-  if (categories === undefined) {
+  if (items === undefined) {
     return <div>Loading...</div>;
   }
 
@@ -90,17 +111,61 @@ const sortOptions = [
 const Sort = (props) => {
   const [isOpen, setIsOpen] = useState(false);
   const [selected, setSelected] = useState("price-asc");
+  const oldSelected = useRef(selected)
 
-  function onChangeSort(value) {
-    setSelected(value);
-    setIsOpen(false);
+  useEffect(() => {
 
+  }, [selected])
+
+  function applySort(value = selected) {
+    console.log('>> selected', selected)
     if (
       props.onChangeSort !== undefined &&
       typeof props.onChangeSort === "function"
     ) {
       props.onChangeSort(value);
     }
+    setIsOpen(false);
+  }
+
+  function onChangeSort(changeOnSelect = false, value) {
+    setSelected(value);
+    if (changeOnSelect) {
+      applySort(value);
+    }
+  }
+
+  function onReset() {
+    setSelected("price-asc");
+    // Need to close(?)
+    // setIsOpen(false);
+
+    if (
+      props.onChangeSort !== undefined &&
+      typeof props.onChangeSort === "function"
+    ) {
+      props.onChangeSort("price-asc");
+    }
+  }
+
+  function listSortOptions(changeOnSelect = false) {
+    return sortOptions.map((option) => {
+      return (
+        <button
+          key={`sort-option-${option.value}`}
+          className="block w-full text-left flex justify-start items-center gap-x-4"
+          onClick={() => onChangeSort(changeOnSelect, option.value)}
+        >
+          {/* radio button */}
+          <div className="w-6 h-6 block bg-white border-2 border-limeGreen rounded-full overflow-hidden p-1 inline-block">
+            {selected === option.value && (
+              <div className="w-full h-full bg-limeGreen rounded-full" />
+            )}
+          </div>
+          <span className="text-sm">{option.name}</span>
+        </button>
+      );
+    })
   }
 
   return (
@@ -111,34 +176,62 @@ const Sort = (props) => {
           isOpen ? "border-limeGreen" : "border-transparent"
         }`}
       >
-        <p className="text-sm">Sort by</p>
-        <img src={Arrow} alt="Arrow Down" className="pl-5" />
+        <p className="text-sm max-md:text-[18px]">Sort by</p>
+        <img src={Arrow} alt="Arrow Down" className="pl-5 hidden md:block" />
+        <img src={SortByIcon} alt="Arrow Down" className="pl-2 w-10 h-10 md:hidden" />
       </button>
 
       {/* Sort options */}
       <div
-        className={`absolute top-[100%] right-0 grid whitespace-nowrap mt-1 p-6 gap-y-6 border border-grey-300 bg-white z-10 ${
-          isOpen ? "" : "hidden"
-        }`}
+        className={`
+          max-md:fixed max-md:bottom-0 max-md:left-0 max-md:w-full
+          absolute md:top-[100%] md:right-0 z-[100]
+          max-md:bg-black/50 max-md:h-full
+          max-md:flex flex-col justify-end items-center
+          ${isOpen ? "" : "max-md:hidden"}
+        `}
       >
-        {sortOptions.map((option) => {
-          return (
-            <button
-              key={`sort-option-${option.value}`}
-              className="block w-full text-left flex justify-start items-center gap-x-4"
-              onClick={() => onChangeSort(option.value)}
-            >
-              {/* radio button */}
-              <div className="w-6 h-6 block bg-white border-2 border-limeGreen rounded-full overflow-hidden p-1 inline-block">
-                {selected === option.value && (
-                  <div className="w-full h-full bg-limeGreen rounded-full" />
-                )}
+        <div
+          className={`
+            w-full grid whitespace-nowrap mt-1 p-6 gap-y-6 border border-grey-300 bg-white z-[100]
+            max-md:rounded-t-2xl
+            ${isOpen ? "" : "hidden"}
+          `}
+        >
+          {/* Mobile: Sort by title, control bar */}
+          <div className="md:hidden">
+            <div className="grid grid-cols-3 text-md">
+              <div className="col text-left">
+                <button className="text-info" onClick={() => {
+                  setSelected(oldSelected.current)
+                  setIsOpen(false)
+                  }}>Cancel</button>
               </div>
+              <div className="col text-center font-semibold">
+                <p className="font-">
+                  Sort by
+                </p>
+              </div>
+              <div className="col text-right">
+                <button className="text-info" onClick={onReset}>Reset</button>
+              </div>
+            </div>
+          </div>
 
-              <span className="text-sm">{option.name}</span>
-            </button>
-          );
-        })}
+          {/* Sort options */}
+          <div className="grid gap-y-4 md:hidden">
+            {listSortOptions(false)}
+          </div>
+
+          <div className="grid gap-y-4 hidden md:grid">
+            {listSortOptions(true)}
+          </div>
+
+          {/* Mobile: Apply sort buttons */}
+          <div className="md:hidden mt-0">
+            <button className="block w-full p-4 text-center bg-black text-white" onClick={()=> applySort()}>Apply</button>
+          </div>
+        </div>
       </div>
     </div>
   );
@@ -148,19 +241,43 @@ const Sort = (props) => {
 // Main component
 const ProductListPage = () => {
   const params = useParams();
+  const navigate = useNavigate();
+  const [pageDetail, setPageDetail] = useState({ name: null, id: null });
   const [sortBy, setSortBy] = useState(["price", "asc"]);
   const [categories, setCategories] = useState({});
   const [products, setProducts] = useState([]);
+  const [collections, setCollections] = useState([]);
 
+  const [selectedCategory, setSelectedCategory] = useState(null);
   const [selectedPermalink, setSelectedPermalink] = useState(null);
 
+  const [selectedCollection, setSelectedCollection] = useState(null);
+
+  const [isLoading, setIsLoading] = useState(true);
+  const [selectedFilter, setSelectedFilter] = useState('')
+
   useEffect(() => {
-    _getAllCategories();
+    setSelectedCategory(null);
+    setSelectedPermalink(null);
+    _getCollections()
+  }, [params])
+
+  useEffect(() => {
+    _getCategoryPageDetail();
+  }, [sortBy, selectedPermalink, selectedCollection, params]);
+
+  useEffect(() => {
+    _getChildrenCategories();
     _getProductByCategory();
-  }, [sortBy, selectedPermalink]);
+  }, [pageDetail]);
+
+  useEffect(() => {
+    setSelectedCollection(null)
+  }, [params.category, params.subCategory]);
 
   // Create category list
   function createCategoryList(fetchedCategories) {
+    console.log('fetchedCategories', fetchedCategories);
     let _categories = {};
 
     // Add to parent category
@@ -205,27 +322,72 @@ const ProductListPage = () => {
     return _categories;
   }
 
-  async function _getAllCategories() {
-    const result = await getAllCategory();
+  async function _getCategoryPageDetail() {
+    const categories = await getAllCategory();
+    const currentCategory = categories.find(category => category.permalink === params.category);
 
-    setCategories(createCategoryList(result));
+    if (currentCategory) {
+      setPageDetail(currentCategory);
+    }
+
+    if (params.subCategory) {
+      const subCategory = categories.find(category => category.permalink === params.subCategory);
+      setSelectedPermalink(subCategory.permalink);
+      setSelectedCategory(subCategory.name);
+      setSelectedFilter(subCategory.id);
+    } else {
+      setSelectedFilter(currentCategory.id);
+    }
   }
 
   async function _getProductByCategory() {
+    setIsLoading(true);
     const result = await getProductByCategory(
-      selectedPermalink === null ? [] : [selectedPermalink],
+      selectedCollection,
+      selectedPermalink === null ? [params.category] : [selectedPermalink],
       sortBy[0],
       sortBy[1]
     );
 
     setProducts(result);
+    setIsLoading(false);
   }
 
-  function onSelectFilter(permalink) {
-    setSelectedPermalink(permalink);
+  async function _getChildrenCategories() {
+    if (pageDetail.id !== null) {
+      const categories = await getChildrenCategories(pageDetail.id);
+
+      const categoriesList = createCategoryList(
+        [{ ...pageDetail, name: 'All products' }, ...categories]
+          .map((category) => ({ ...category, parentId: null }))
+      );
+      setCategories(categoriesList);
+    }
+  }
+
+  async function _getCollections() {
+    const result = await getCollections();
+
+    setCollections([
+      { id: '', name: 'Collections', children: result },
+    ])
+  }
+
+  function onSelectFilter(id, permalink, categoryName) {
+    setSelectedFilter(id)
+    setSelectedPermalink(permalink, categoryName);
+    
+    if (permalink === pageDetail.permalink) {
+      navigate(`/products/${permalink}`);
+      // setSelectedCategory(null);
+    } else {
+      navigate(`/products/${params.category}/${permalink}`);
+      // setSelectedCategory(categoryName);
+    }
   }
 
   function onSort(sortBy) {
+    // console.log('>> Sort by', sortBy)
     switch (sortBy) {
       case "price-asc":
         setSortBy(["price", "asc"]);
@@ -239,51 +401,102 @@ const ProductListPage = () => {
     }
   }
 
+  function renderHeadingTitle() {
+    const mainPageName = checkYourGrammarChangeFromPruralToSingular(pageDetail.name);
+    if (mainPageName === null) {
+      return "Loading...";
+    }
+
+    if (selectedCategory === null) {
+      return `${mainPageName}`;
+    }
+
+    return `${mainPageName}'s ${selectedCategory}`;
+  }
+
+  function checkYourGrammarChangeFromPruralToSingular(text) {
+    switch (text) {
+      case 'Ladies':
+        return 'Lady';
+      case 'Men':
+        return 'Man';
+      case 'Kids':
+        return 'Kid';
+      case 'Accessories':
+        return 'Accessory';
+      case 'Women':
+        return 'Woman';
+      default:
+        return text;
+    }
+  }
+
   return (
     <>
-      <div className="container mx-auto my-24">
+      <div className="container mx-auto max-md:mt-6 my-24">
         <div className="grid grid-cols-4 gap-x-10">
-          <div className="col-span-1">
+          <div className="col-span-1 max-md:hidden">
             {/* Filter */}
             <Filter
-              categories={Object.values(categories)}
+              items={Object.values(categories)}
               onSelectFilter={onSelectFilter}
+              selectedFilter={selectedFilter}
+            />
+
+            <Filter
+              items={collections}
+              onSelectFilter={(id, permalink, name) => {
+                setSelectedCollection(permalink)
+              }}
+              selectedFilter={selectedCollection}
             />
           </div>
 
-          <div className="md:col-span-3">
-            <div className="grid grid-cols-5">
-              <div className="md:col-span-4">
-                <h1 className="text-3xl font-bold">{`{{collection.name}}`} Clothing</h1>
-                {/* <p>Sort by: {sortBy}</p> */}
+          <div className="md:col-span-3 col-span-full">
+            <div className="grid md:grid-cols-5">
+              <div className="md:col-span-4 flex justify-start items-center">
+                <h1 className="text-3xl font-bold">{renderHeadingTitle()}</h1>
               </div>
-              <div className="md:col-span-1">
-                <Sort
-                  onChangeSort={(sort) => {
-                    onSort(sort);
-                  }}
-                />
+              <div className="md:col-span-1 text-right flex justify-end items-center">
+                <Sort onChangeSort={onSort} />
               </div>
             </div>
 
             {/* Products */}
-            <div className="grid md:grid-cols-3 gap-x-10 gap-y-10 mt-10">
+            <div className="grid grid-cols-1 md:grid-cols-3 gap-x-10 gap-y-10 mt-10 relative">
+              
+              {
+                isLoading && (
+                  <div className="bg-black/50 w-full h-full absolute z-10 backdrop-blur-md p-4 min-h-16 col-span-full">
+                      <p className="text-center text-white font-bold animate-pulse">Loading products...</p>
+                  </div>
+                )
+              }
+
+              {
+                products.length === 0 && !isLoading && (
+                  <div className="col-span-1">
+                    <p className="text-center">No products found</p>
+                  </div>
+                )
+              }
               {
                 products.map((product) => (
-                  <ProductCard
-                    key={product.id}
-                    name={product.name}
-                    description={product.description}
-                    image={product.imageUrls.length > 0 ? product.imageUrls[0] : ''}
-                    rating={product.ratings}
-                    price={product.price}
-                    promotionPrice={product.promotionalPrice !== undefined ? product.promotionalPrice : 0}
-                    isPromotion={product.promotionalPrice !== undefined}
-                    permalink={product.permalink}
-                    // price={390}
-                    // promotionPrice={290}
-                    // isPromotion={true}
-                  />
+                  <div className="col-span-1" key={`product-card-${product.id}`}>
+                    <ProductCard
+                      name={product.name}
+                      description={product.description}
+                      image={product.imageUrls.length > 0 ? product.imageUrls[0] : ''}
+                      rating={product.ratings}
+                      price={product.price}
+                      promotionPrice={product.promotionalPrice !== undefined ? product.promotionalPrice : 0}
+                      isPromotion={product.promotionalPrice !== product.price}
+                      permalink={product.permalink}
+                      // price={390}
+                      // promotionPrice={290}
+                      // isPromotion={true}
+                    />
+                  </div>
                 ))
               }
             </div>
